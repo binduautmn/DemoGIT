936222e93b340e4872f730bb6f79d678686b9687
Govardhan Nagunoori <Govardhan_N@ad.infosys.com>
2018-11-15 16:34:22 -0500

    new

--- src/main/java/com/qa/util/ExcelReader.java
+++ src/main/java/com/qa/util/ExcelReader.java
@@ -2,7 +2,11 @@ package com.qa.util;
 
 public class ExcelReader {
 	public void readExcel() {
+<<<<<<< HEAD
 		System.out.println("read this excel");
+=======
+		System.out.println("read this 2 excel");
+>>>>>>> new
 	}
 
 }
