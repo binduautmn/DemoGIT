dc2a2b089035c80eb0fde852ef5d4a1e7b37a237
Govardhan Nagunoori <Govardhan_N@ad.infosys.com>
2018-11-15 17:07:51 -0500

    tested

--- src/main/java/com/qa/util/ExcelReader.java
+++ src/main/java/com/qa/util/ExcelReader.java
@@ -2,11 +2,7 @@ package com.qa.util;
 
 public class ExcelReader {
 	public void readExcel() {
-<<<<<<< HEAD
-		System.out.println("read this excel");
-=======
-		System.out.println("read this 2 excel");
->>>>>>> new
+		System.out.println("read this sexcel");
 	}
 
 }
